9d2ae0f740c6fa4dbd914bc38f411ce3c0342883
Miguii-6 <mmarinomartinez@danielcastelao.org>
2023-12-18 17:02:59 +0100

    clase Data completada

--- app/src/main/java/com/example/simondice/Data.kt
+++ app/src/main/java/com/example/simondice/Data.kt
@@ -1,6 +1,38 @@
 package com.example.simondice
 
-
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.Color
 
 object Data {
+    var ronda = mutableStateOf(0);
+    var secuence = mutableListOf<Int>();
+    var secuenceUser = mutableListOf<Int>();
+    var score = mutableStateOf(0);
+    var estado = State.START;
+
+
+    var colores = listOf(
+        Colors.ROJO.color,
+        Colors.AZUL.color,
+        Colors.AMARILLO.color,
+        Colors.VERDE.color
+    )
+
+    //Aun no se para que usar esto
+    var numColores = Colors.values()
+    var colorPath: Color = Color.White
+
+    enum class State {
+        START, SEQUENCE, WAITING, CHECKING, FINISHED
+    }
+
+    enum class Colors(val color: MutableState<Color>,  val colorName: String) {
+        ROJO(mutableStateOf(Color.Red), "ROJO"),
+        AZUL(color = mutableStateOf(Color.Blue), "AZUL"),
+        AMARILLO(color = mutableStateOf(Color.Yellow), "AMARILLO"),
+        VERDE(color = mutableStateOf(Color.Green), "VERDE"),
+
+    }
+
 }
