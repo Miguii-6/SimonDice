d035b1ee2bd06e067613cc9dfadefccc8d64523f
Miguii-6 <mmarinomartinez@danielcastelao.org>
2023-12-18 17:22:44 +0100

    clase MainActivity completada

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -3,13 +3,8 @@ package com.example.simondice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.simondice.ui.theme.Greeting
 import com.example.simondice.ui.theme.SimonDiceTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,29 +13,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             SimonDiceTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Surface{
+                    Greeting(miModel = MyViewModel())
+            }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SimonDiceTheme {
-        Greeting("Android")
     }
 }
