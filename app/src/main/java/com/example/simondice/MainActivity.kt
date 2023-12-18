7867be4e77a649739a41515933a7fc65b749c211
Miguii-6 <mmarinomartinez@danielcastelao.org>
2023-12-18 17:38:42 +0100

    Creo el README.md

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -6,17 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
 import com.example.simondice.ui.theme.Greeting
 import com.example.simondice.ui.theme.SimonDiceTheme
-
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SimonDiceTheme {
                 // A surface container using the 'background' color from the theme
-                Surface{
-                    Greeting(miModel = MyViewModel())
-            }
+                Surface {
+                    Greeting(miModel = MyViewModel()) // Pasando el ViewModel a la interfaz
+
+                }
             }
         }
     }
 }
+
